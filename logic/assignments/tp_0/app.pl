51a4c0df8cacef63951f5c9293da66182e16b84a
nicolau <marcosnicolau2003@gmail.com>
2023-06-01 15:41:31 -0300

    first logic assignment

---
+++ logic/assignments/tp_0/app.pl
@@ -0,0 +1,35 @@
+nacio(nacho,bsas,26,6,1989).
+nacio(tito,bsas,26,4,2000).
+nacio(tita,bsas,23,3,2000).
+nacio(lola,bsas,1,1,2001).
+
+
+horoscopo_pdep(P, funcional) :-
+    nacio(P, _, Dia, Mes, _),
+    (
+        (Mes = 3, Dia >= 23); 
+        (Mes > 3, Mes < 5);
+        (Mes = 5, Dia =< 29)
+    ).
+
+horoscopo_pdep(P, logico) :-
+    nacio(P, _, Dia, Mes, _),
+    (
+        (Mes = 5, Dia > 29);
+        (Mes >= 6, Mes < 8);
+        (Mes = 8, Dia =< 2)
+    ).
+
+horoscopo_pdep(P, oop) :-
+    nacio(P, _, Dia, Mes, _),
+    (
+        (Mes = 8, Dia =< 2);
+        (Mes > 8, Mes < 11);
+        (Mes = 11, Dia =< 30)
+    ).
+
+horoscopo_pdep(P, sinSigno) :-
+    nacio(P, _, Dia, Mes, _),
+    \+ horoscopo_pdep(P, funcional),
+    \+ horoscopo_pdep(P, logico),
+    \+ horoscopo_pdep(P, oop).
---
+++ logic/assignments/tp_0/readme.md
@@ -0,0 +1,8 @@
+# TP Horóscopo PdeP
+
+Según en que parte del año nació una persona, le toca un paradigma diferente:
+
+-   Funcional: desde el inicio de clases hasta ayer, funcional
+-   Lógico: desde hoy hasta las vacaciones de invierno, logico
+-   OOP(Object Oriented Programming): segundo cuatrimestre
+-   Sin signo: para el resto del año.
